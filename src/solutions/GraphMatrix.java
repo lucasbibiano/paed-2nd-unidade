a031de6332b20c072a97a661c2e56158aa0b1611
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-06-06 23:56:23 -0300

    adicionando representacao matricial ao grafo

---
+++ src/solutions/GraphMatrix.java
@@ -0,0 +1,40 @@
+package solutions;
+
+public class GraphMatrix {
+	private int n;
+	
+	int[][] nodes = new int[n][n];
+	
+	int predecessors[];
+	
+	public GraphMatrix(int numberOfNodes) {
+		this.n=numberOfNodes;
+	}
+
+	public void addEdge(int i, int j, int weight ) {
+		this.nodes[i][j]=weight;
+	}
+	
+	public String shortest(int i, int j) {
+		return null;
+		
+	}
+	
+	public String path(int i, int j) {
+		return null;
+	}
+
+	public boolean auxPath(int i, int j) {	
+		return true;
+	}
+	
+	private void printByPredecessor(int j) {
+
+	}
+	
+	public void printPred() {
+
+	}
+	
+	
+}
